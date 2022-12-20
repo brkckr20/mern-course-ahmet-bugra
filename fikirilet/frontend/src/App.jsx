c5320bc4ec8e06ec14fc59124ad662ac08ce975e
Burak Çakır <burak2043@gmail.com>
2022-12-20 16:05:49 +0300

    backend edited

--- fikirilet/frontend/src/App.jsx
+++ fikirilet/frontend/src/App.jsx
@@ -2,40 +2,55 @@ import { useState, useEffect } from 'react'
 import axios from 'axios';
 
 function App() {
-  const [message, setMessage] = useState({
-    adSoyad: "",
-    email: "",
-    fikirTuru: "",
-    aciklama: "",
-  });
 
-  const handleChange = (e) => {
-    setMessage({
-      [e.target.name]: e.target.value
-    })
+  // const [message, setMessage] = useState({
+  //   adSoyad: "",
+  //   email: "",
+  //   fikirTuru: "",
+  //   aciklama: "",
+  // });
+
+  // const handleChange = (e) => {
+  //   setMessage({
+  //     [e.target.name]: e.target.value
+  //   })
+  // }
+
+
+  const [adSoyad, setAdSoyad] = useState("")
+  const [email, setEmail] = useState("")
+  const [fikirTuru, setfikirTuru] = useState("")
+  const [aciklama, setAciklama] = useState("")
+
+  const values = {
+    adSoyad,
+    email,
+    fikirTuru,
+    aciklama
   }
 
-  const handleFormSubmit = (e) => {
+  const handleFormSubmit = async (e) => {
     e.preventDefault();
-    console.log(message);
+    const res = await axios.post(`http://localhost:3001/fikirkaydet`, values);
+    console.log(res);
   }
   return (
     <div>
       <h1 className='text-center text-3xl font-semibold mb-5'>Fikir Ilet Uygulamasi</h1>
       <div className='mx-auto w-1/4'>
         <form onSubmit={handleFormSubmit}>
-          <input value={message.adSoyad} name="adSoyad" onChange={handleChange} className='border border-gray-300 pl-1 rounded w-full mb-1' placeholder='Ad soyad' type="text" />
-          <input value={message.email} name="email" className='border border-gray-300 pl-1 rounded w-full mb-1' placeholder='e-mail adresi' type="email" />
-          <select onChange={handleChange} value={message.fikirTuru} name="fikirTuru" className='border border-gray-300 pl-1 rounded w-full'>
-            <option value="Öneri">Öneri</option>
-            <option value="Öneri">Hata Bildirim</option>
-            <option value="Öneri">Şikayet</option>
-            <option value="Öneri">İstek</option>
+          <input value={adSoyad} name="adSoyad" onChange={(e) => setAdSoyad(e.target.value)} className='p-2 border border-gray-300 pl-1 rounded w-full mb-1' placeholder='Ad soyad' type="text" />
+          <input value={email} name="email" onChange={(e) => setEmail(e.target.value)} className='p-2 border border-gray-300 pl-1 rounded w-full mb-1' placeholder='e-mail adresi' type="email" />
+          <select value={fikirTuru} onChange={(e) => setfikirTuru(e.target.value)} name="fikirTuru" className='p-2 border border-gray-300 pl-1 rounded w-full'>
+            <option value="">Seçiniz</option>
+            <option value="oneri">Öneri</option>
+            <option value="hata">Hata Bildirim</option>
+            <option value="sikayet">Şikayet</option>
+            <option value="istek">İstek</option>
           </select>
-          <textarea name='aciklama' value={message.aciklama} onChange={handleChange} className='border border-gray-300 pl-1 rounded w-full mt-1 h-28'>
-
+          <textarea name='aciklama' onChange={(e) => setAciklama(e.target.value)} value={aciklama} className='border border-gray-300 pl-1 rounded w-full mt-1 h-28'>
           </textarea>
-          <button type='submit' className='bg-green-600 w-full mt-1 text-white rounded hover:bg-green-800 duration-150'>
+          <button type='submit' className='bg-green-600 w-full mt-1 text-white rounded hover:bg-green-800 duration-150 p-2'>
             Kaydet
           </button>
         </form>
--- fikirilet/server/app.js
+++ fikirilet/server/app.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+// const bodyParser = require("body-parser");
 
 app.use(cors());
+app.use(express.json());
+
 
 app.get("/kullanici", (req, res) => {
     res.send(
@@ -10,6 +13,13 @@ app.get("/kullanici", (req, res) => {
             message: "Hello React"
         }
     )
+});
+
+app.post("/fikirkaydet", (req, res) => {
+    console.log(req.body);
+    res.send(req.body)
 })
+
+// app.post()
 
 app.listen(3001)
